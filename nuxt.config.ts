762b172ab1fb7f593d0fbeec31f4cbd103bdf721
joncorv <joncorv@gmail.com>
2026-02-18 14:06:32 -0800

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/ui',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
