1400ed64cf14bd9db4d0d30abde85460f5c5c9ab
Jonathan E. Corriveau <joncorv@gmail.com>
2026-02-18 22:29:45 -0800

    base project page added

---
+++ app/pages/animation/[slug].vue
@@ -0,0 +1,16 @@
+<script setup>
+const slug = useRoute().params.slug
+const { data: project } = await useAsyncData(`animation-${slug}`, () => {
+  return queryCollection('animation').path(`/animation/${slug}`).first()
+})
+</script>
+
+<template>
+  <h1> {{ project.title }} </h1>
+  <h3> {{ project.description }} </h3>
+  <h1> {{ project.client }} </h1>
+  <h1> {{ project.date }} </h1>
+  <h1> {{ project.tools }} </h1>
+  <h1> {{ project.tags }} </h1>
+  <ContentRenderer :value="project" />
+</template>
