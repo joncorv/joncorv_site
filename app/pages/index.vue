16af312b9ac72a825daa6f9b292320328dc5be68
Jonathan E. Corriveau <joncorv@gmail.com>
2026-02-18 22:29:54 -0800

    homepage added

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { data: projects } = await useAsyncData(() => queryCollection('animation').all())
+</script>
+
+<template>
+  <!-- <ContentRenderer v-if="projects" v-for="project in projects" :value="project" :key="project.id" /> -->
+  <h1>All My Projects</h1>
+  <div v-if="projects">
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <NuxtLink :to="project.path">{{ project.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <div v-else>Projects not found!</div>
+</template>
