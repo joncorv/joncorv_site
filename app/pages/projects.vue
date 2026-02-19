a7ffce2feca6bfb08ddecaf4649466dee5bba6a9
Jonathan E. Corriveau <joncorv@gmail.com>
2026-02-18 21:29:31 -0800

    projects query added

---
+++ app/pages/projects.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data: projects } = await useAsyncData(() => queryCollection('animation').all())
+</script>
+
+<template>
+
+  <ContentRenderer v-if="projects" v-for="project in projects" :value="project" />
+
+  <div v-else>Home not found</div>
+</template>
