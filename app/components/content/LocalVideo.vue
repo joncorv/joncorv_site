845b16798f15a74a18fc33decd3c795c7ba48308
Jonathan E. Corriveau <joncorv@gmail.com>
2026-02-22 10:58:14 -0800

    renamed component from  Video -> LocalVideo

---
+++ app/components/content/LocalVideo.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  poster: {
+    type: String,
+    default: undefined,
+  },
+})
+</script>
+
+<template>
+  <div class="video_wrapper">
+    <video :src="src" :poster="poster" loop playsinline controls />
+  </div>
+</template>
+
+<style scoped>
+.video_wrapper {
+  border-radius: 0.75rem;
+  overflow: hidden;
+}
+
+video {
+  display: block;
+  width: 100%;
+}
+</style>
