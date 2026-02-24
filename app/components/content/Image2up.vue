f1e8459b1ddc1539a5d4104c2b31621cb9197231
Jonathan E. Corriveau <joncorv@gmail.com>
2026-02-23 23:08:54 -0800

    props updated, not quite done

---
+++ app/components/content/Image2up.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+const props = defineProps({
+  imageSource1: {
+    type: String,
+    default: "/images/dummy_image.png",
+  },
+  imageSource2: {
+    type: String,
+    default: "/images/dummy_image.png",
+  },
+  height1: {
+    type: Number,
+    default: 50,
+  },
+  height2: {
+    type: Number,
+    default: 50,
+  },
+});
+
+const height1 = computed(() => `${props.height1}rem`);
+const height2 = computed(() => `${props.height2}rem`);
+</script>
+
+<template>
+  <div class="container">
+    <div
+      class="left_image"
+      :style="{ height: height1 }"
+    >
+      <NuxtImg :src="imageSource1" />
+    </div>
+
+    <div
+      class="right_image"
+      :style="{ height: height2 }"
+    >
+      <NuxtImg :src="imageSource2" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
+  margin-bottom: 3rem;
+  margin-top: 3rem;
+}
+
+/* .right_image { */
+/*   margin-top: 7rem; */
+/* } */
+
+.left_image,
+.right_image {
+  flex: 1;
+  min-width: 0;
+  display: flex;
+}
+
+.left_image :deep(img),
+.right_image :deep(img) {
+  flex: 1;
+  min-width: 0;
+  min-height: 0;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 1.5rem;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
+  display: block;
+}
+</style>
