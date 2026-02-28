a30840cab6f9b5616b5ba8ed8fe3ef47d67b8665
joncorv <joncorv@gmail.com>
2026-02-27 17:14:11 -0800

    made great progress on the type design here

--- app/components/ProjectHero.vue
+++ app/components/ProjectHero.vue
@@ -5,11 +5,25 @@
 <template>
   <div class="container">
     <p class="type">
-      Invariably, something <u><span class="type_italics">very nice</span></u> will go here.
+
+      <span class="type">I'm Jonathan Corriveau, a San Francisco-based </span>
       <br>
-      for now, it remains blank.
-      <!-- <br> -->
-      <!-- like my <u><span class="type_italics">cold, dark soul.</span></u> -->
+      <u><span class="type_italics">CG Artist</span></u>
+      <span class="type">, </span>
+      <u><span class="type_italics">coder</span></u>
+      <span class="type_italics">, & </span>
+      <u><span class="type_italics">perpetual student</span></u>.
+      <br>
+      <br>
+      <span class="type">I work at</span>
+      <span class="pill">WTBR</span>
+      <!-- <span class="pill">BRAND DESIGN</span> -->
+      <span class="type"> where I make </span>
+      <u><span class="type_italics">art with code</span></u>
+      <span class="type">,</span>
+      <br>
+      <span class="type"> and find joy in bringing others' visions to life.</span>
+
     </p>
   </div>
 </template>
@@ -19,8 +33,6 @@
   width: 100%;
   aspect-ratio: 16 / 7;
   border-radius: 1.5rem;
-  /* background-color: var(--ui-bg-elevated); */
-  border: 2px solid var(--ui-border);
   margin-bottom: 1.5rem;
   display: flex;
   justify-content: center;
@@ -32,13 +44,32 @@
 .type {
   font-family: "Playfair Display";
   font-weight: 300;
-  font-size: 3rem;
-  text-align: center;
+  font-size: 2.5rem;
+  text-align: left;
   margin: auto;
 
 }
 
 .type_italics {
   font-style: italic;
+}
+
+.pill {
+  font-weight: 250;
+  font-size: 1.25rem;
+  text-align: center;
+  text-anchor: middle;
+  border: 2px solid var(--ui-bg-elevated);
+  border-radius: 5rem;
+  padding: .3rem 1rem .3rem 1rem;
+  vertical-align: middle;
+  margin: 0 0rem 0 1rem;
+}
+
+u {
+  text-decoration: underline;
+  text-decoration-thickness: 2px;
+  text-underline-offset: 6px;
+  /* text-decoration-color: red; */
 }
 </style>
