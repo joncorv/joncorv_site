56de19d86a76356264288bcab0289a0fa1e3218a
joncorv <joncorv@gmail.com>
2026-03-02 16:56:35 -0800

    ProjectHero updated with new pill and single-line setup

--- app/components/ProjectHero.vue
+++ app/components/ProjectHero.vue
@@ -4,27 +4,35 @@
 
 <template>
   <div class="container">
-    <p class="type">
-
+    <div class="type">
       <span class="type">I'm Jonathan Corriveau, a San Francisco-based </span>
       <br>
-      <u><span class="type_italics">CG Artist</span></u>
+      <u><span class="type_italics">CG artist</span></u>
       <span class="type">, </span>
       <u><span class="type_italics">coder</span></u>
+
       <span class="type_italics">, & </span>
       <u><span class="type_italics">perpetual student</span></u>.
       <br>
       <br>
-      <span class="type">I work at</span>
-      <span class="pill">WTBR</span>
-      <!-- <span class="pill">BRAND DESIGN</span> -->
-      <span class="type"> where I make </span>
-      <u><span class="type_italics">art with code</span></u>
-      <span class="type">,</span>
+      <div class="single-line">
+        <span class="type">I work at </span>
+        <div class="pill">
+          <NuxtImg
+            src="/images/logos/wtbr_white_v3.png"
+            width="2.5rem"
+            class="h-5"
+          />
+          WTBR
+        </div>
+        <span class="type"> where I make </span>
+        <span class="type"> &nbsp;</span>
+        <u><span class="type_italics">art with code</span></u>
+        <span class="type">,</span>
+      </div>
       <br>
       <span class="type"> and find joy in bringing ideas to life.</span>
-
-    </p>
+    </div>
   </div>
 </template>
 
@@ -43,7 +51,7 @@
 .type_italics,
 .type {
   font-family: "Playfair Display";
-  font-weight: 300;
+  font-weight: 500;
   font-size: 2.5rem;
   text-align: left;
   margin: auto;
@@ -51,6 +59,14 @@
 
 .type_italics {
   font-style: italic;
+  font-weight: 900;
+}
+
+.single-line {
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .pill {
@@ -62,12 +78,20 @@
   border-radius: 5rem;
   padding: .3rem 1rem .3rem 1rem;
   vertical-align: middle;
-  margin: 0 0rem 0 1rem;
+  margin: 0 0rem 0 0rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+  height: 3rem;
+  margin-left: 0.6rem;
+  margin-right: 0.6rem;
 }
 
 u {
   text-decoration: underline;
-  text-decoration-thickness: 2px;
+  text-decoration-thickness: 1.5px;
   text-underline-offset: 6px;
   /* text-decoration-color: red; */
 }
