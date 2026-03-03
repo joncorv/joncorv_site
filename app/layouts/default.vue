50f29185b4b69d7effd993bb043d001f75f67c62
joncorv <joncorv@gmail.com>
2026-03-02 16:57:04 -0800

    removed pill on header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -28,11 +28,11 @@ const items = computed<NavigationMenuItem[]>(() => [{
 <template>
   <UHeader>
     <template #title>
-      <div class="pill">
-        <p class="title_my_name">
-          JONCORV
-        </p>
-      </div>
+      <!-- <div class="pill-my-name"> -->
+      <p class="title-my-name">
+        JONCORV
+      </p>
+      <!-- </div> -->
       <!-- <NuxtImg src="/images/logo_v01.png" height="200px" class="h-7" /> -->
     </template>
 
@@ -41,14 +41,27 @@ const items = computed<NavigationMenuItem[]>(() => [{
     <template #right>
       <UColorModeButton />
 
-      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
-        <UButton color="neutral" variant="ghost" to="https://github.com/nuxt/ui" target="_blank"
-          icon="i-simple-icons-github" aria-label="GitHub" />
+      <UTooltip
+        text="Open on GitHub"
+        :kbds="['meta', 'G']"
+      >
+        <UButton
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/nuxt/ui"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+        />
       </UTooltip>
     </template>
 
     <template #body>
-      <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
+      <UNavigationMenu
+        :items="items"
+        orientation="vertical"
+        class="-mx-2.5"
+      />
     </template>
   </UHeader>
 
@@ -67,30 +80,48 @@ const items = computed<NavigationMenuItem[]>(() => [{
     <!-- <UNavigationMenu :items="items" variant="link" /> -->
 
     <template #right>
-      <UButton icon="i-simple-icons-instagram" color="neutral" variant="ghost" to="https://www.instagram.com/joncorv/"
-        target="_blank" aria-label="Instagram" />
-      <UButton icon="i-simple-icons-linkedin" color="neutral" variant="ghost" to="https://www.linkedin.com/in/joncorv/"
-        target="_blank" aria-label="LinkedIn" />
-      <UButton icon="i-simple-icons-github" color="neutral" variant="ghost" to="https://github.com/joncorv"
-        target="_blank" aria-label="GitHub" />
+      <UButton
+        icon="i-simple-icons-instagram"
+        color="neutral"
+        variant="ghost"
+        to="https://www.instagram.com/joncorv/"
+        target="_blank"
+        aria-label="Instagram"
+      />
+      <UButton
+        icon="i-simple-icons-linkedin"
+        color="neutral"
+        variant="ghost"
+        to="https://www.linkedin.com/in/joncorv/"
+        target="_blank"
+        aria-label="LinkedIn"
+      />
+      <UButton
+        icon="i-simple-icons-github"
+        color="neutral"
+        variant="ghost"
+        to="https://github.com/joncorv"
+        target="_blank"
+        aria-label="GitHub"
+      />
     </template>
   </UFooter>
 </template>
 
 <style scoped>
-.pill {
+.pill-my-name {
   font-weight: 250;
-  font-size: 1.25rem;
+  font-size: 1.1rem;
   text-align: center;
   text-anchor: middle;
-  border: 2px solid var(--ui-text-highlighted);
-  border-radius: 0.25rem;
-  padding: .1rem 0.5rem .1rem 0.5rem;
+  border: 1.7px solid var(--ui-text-highlighted);
+  border-radius: 0.2rem;
+  padding: 0rem 0.2rem 0rem 0.2rem;
   vertical-align: middle;
   margin: 0 0rem 0 1rem;
 }
 
-.title_my_name {
+.title-my-name {
   font-family: "JetBrains Mono", monospace;
   /* font-family: "Space Mono", monospace; */
   font-size: 1em;
