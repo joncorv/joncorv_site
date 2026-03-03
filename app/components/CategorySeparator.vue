d62a3cfd6f6f0d69c6a45e4e61e49b839ffd9e26
joncorv <joncorv@gmail.com>
2026-03-02 16:56:11 -0800

    CategorySeparator component added for reusability

---
+++ app/components/CategorySeparator.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Category Separator",
+  },
+});
+</script>
+
+<template>
+  <hr class="horiz-rule">
+
+  <p class="text-category">
+    {{ props.title }}
+  </p>
+</template>
+
+<style scoped>
+.horiz-rule {
+  margin-top: 1rem;
+  border-color: var(--ui-border-accented);
+}
+
+.text-category {
+  font-size: 0.85rem;
+  color: var(--ui-text-muted);
+  margin-top: 0.5rem;
+  margin-bottom: 2rem;
+}
+</style>
