86a6e6d684623795160a755def3b5f3c994f1d46
Jonathan Corriveau <joncorv@gmail.com>
2026-03-17 17:02:27 -0700

    all-projects page added!

---
+++ app/pages/animation/all-projects.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UContainer>
+    <div class="page">
+      <!-- <LandingHero /> -->
+      <!-- <CategorySeparator title="ALL SELECTED WORKS" /> -->
+      <SelectedWorks
+        :num-projects="31"
+        :landing-page="false"
+      />
+    </div>
+  </UContainer>
+</template>
+
+<style scoped>
+.page {
+  padding: 1rem 0 6rem;
+}
+</style>
