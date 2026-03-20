68c20b87e8c394d0cab6049d0356b389055d8ef0
Jonathan E. Corriveau <joncorv@gmail.com>
2026-03-20 10:28:20 -0700

    landingHero updated

--- app/components/LandingHero.vue
+++ app/components/LandingHero.vue
@@ -46,6 +46,26 @@
       <span class="type-normal">,</span>
       <!-- <br> -->
       <span class="type-normal"> and find joy in bringing ideas to life.</span>
+
+      <div class="cust-break" />
+
+      <span class="type-normal">Recently I built a cross platform desktop app called </span>
+      <NuxtLink
+        to="https://github.com/joncorv/filera"
+        target="_blank"
+        external
+        no-prefetch
+      >
+        <div class="pill-2">
+          <!-- <Icon -->
+          <!--   name="local-icons:buffalo" -->
+          <!--   class="buffalo-image" -->
+          <!-- /> -->
+          <!---->
+          FILERA
+        </div>
+      </NuxtLink>
+      <span class="type-normal">. It's a powerful cross-platform file renaming app built in Rust 🦀 & Tauri </span>
     </div>
   </div>
 </template>
@@ -88,7 +108,7 @@
 
 .pill {
   font-family: var(--font-mono);
-  font-weight: 250;
+  font-weight: 700;
   font-size: 0.55em;
   text-align: center;
   text-anchor: middle;
@@ -103,23 +123,47 @@
   width: 6em;
   margin-left: 0.2em;
   margin-right: 0.2em;
-
   border-radius: 5rem;
   border-style: solid;
   border-color: var(--ui-text-highlighted);
   border-width: .12em;
-
-  /* background: linear-gradient(0deg, transparent, transparent); */
-  /* color: var(--ui-text-highlighted); */
-
   background: linear-gradient(0deg, var(--ui-text-highlighted), var(--ui-text-toned));
   color: var(--ui-bg-elevated);
-
   transition: background 0.15s ease, color 0.15s ease;
-
 }
 
 .pill:hover {
+  background: linear-gradient(0deg, transparent, transparent);
+  color: var(--ui-text-highlighted);
+}
+
+.pill-2 {
+  font-family: var(--font-mono);
+  font-weight: 700;
+  font-size: 0.55em;
+  text-align: center;
+  text-anchor: middle;
+  padding: .3em 1em .3em 1em;
+  vertical-align: middle;
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4em;
+  height: 2em;
+  width: 5em;
+  margin-left: 0.2em;
+  margin-right: 0.2em;
+  border-radius: 5rem;
+  border-style: solid;
+  border-color: var(--ui-text-highlighted);
+  border-width: .12em;
+  background: linear-gradient(0deg, var(--ui-text-highlighted), var(--ui-text-toned));
+  color: var(--ui-bg-elevated);
+  transition: background 0.15s ease, color 0.15s ease;
+}
+
+.pill-2:hover {
   background: linear-gradient(0deg, transparent, transparent);
   color: var(--ui-text-highlighted);
 }
