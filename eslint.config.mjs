55c4fe116f1eb8ec34c5a5fb8945a31f78fa68da
Jonathan Corriveau <joncorv@gmail.com>
2026-03-20 14:10:50 -0700

    eslint update --- single lines now can go as long as needed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,13 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt({
   rules: {
     "vue/no-multiple-template-root": "off",
+
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: Infinity,
+        multiline: 1,
+      },
+    ],
   },
 });
