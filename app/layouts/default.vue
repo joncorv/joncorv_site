2a3a7b43d8cfa8b0a59735afff5f8c114aa7767d
Jonathan Corriveau <joncorv@gmail.com>
2026-03-26 21:35:22 -0700

    header updated

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,67 +1,28 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from "@nuxt/ui";
 
-const route = useRoute();
-
-const items = computed<NavigationMenuItem[]>(() => [{
-  label: "Docs",
-  to: "/docs/getting-started",
-  icon: "i-lucide-book-open",
-  active: route.path.startsWith("/docs/getting-started"),
-}, {
-  label: "Components",
-  to: "/docs/components",
-  icon: "i-lucide-box",
-  active: route.path.startsWith("/docs/components"),
-}, {
-  label: "Figma",
-  icon: "i-simple-icons-figma", to: "https://go.nuxt.com/figma-ui",
-  target: "_blank",
-}, {
-  label: "Releases",
-  icon: "i-lucide-rocket",
-  to: "https://github.com/nuxt/ui/releases",
-  target: "_blank",
-}]);
 </script>
 
 <template>
-  <UHeader>
+  <UHeader :toggle="false">
     <template #title>
-      <!-- <div class="pill-my-name"> -->
       <p class="title-my-name">
         JONCORV
       </p>
-      <!-- </div> -->
-      <!-- <NuxtImg src="/images/logo_v01.png" height="200px" class="h-7" /> -->
     </template>
-
-    <!-- <UNavigationMenu :items="items" /> -->
 
     <template #right>
       <UColorModeButton />
 
-      <UTooltip
-        text="Open on GitHub"
-        :kbds="['meta', 'G']"
-      >
+      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
         <UButton
           color="neutral"
           variant="ghost"
-          to="https://github.com/nuxt/ui"
+          to="https://github.com/joncorv"
           target="_blank"
           icon="i-simple-icons-github"
           aria-label="GitHub"
         />
       </UTooltip>
-    </template>
-
-    <template #body>
-      <UNavigationMenu
-        :items="items"
-        orientation="vertical"
-        class="-mx-2.5"
-      />
     </template>
   </UHeader>
 
