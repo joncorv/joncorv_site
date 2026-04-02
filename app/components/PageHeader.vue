4112f97d2ac8ca89da88cc85cd0ed6958310f3c0
Jonathan E. Corriveau <joncorv@gmail.com>
2026-04-02 15:11:38 -0500

    PageHeader component has working dummy implimentation of hiding header

--- app/components/PageHeader.vue
+++ app/components/PageHeader.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import type { DropdownMenuItem } from "@nuxt/ui";
+import { onMounted, onUnmounted } from "vue";
+import { useWindowScroll } from "@vueuse/core";
 
 const items = ref<DropdownMenuItem[]>([
 
@@ -48,18 +50,77 @@ const items = ref<DropdownMenuItem[]>([
     noPrefetch: true,
     target: "_blank",
   },
-
 ]);
+
+const headerVisible = ref(true);
+
+onNuxtReady(() => {
+  const { y } = useWindowScroll();
+  const prevY = ref(0);
+
+  // fix negative scrolling values
+  const yComputed = computed(() => {
+    if (y.value < 0) {
+      return 0;
+    }
+    else {
+      return y.value;
+    }
+  });
+
+  // every x milliseconds, check to see if  we're heading up or down
+  setInterval(() => {
+    // scrolling down
+    if (yComputed.value > prevY.value + 30) {
+      prevY.value = y.value;
+      // console.log("mounted_tick: Scrolled Down - STATE CHANGE");
+      headerVisible.value = false;
+    }
+
+    // scrolling up
+    else if (yComputed.value < prevY.value) {
+      prevY.value = y.value;
+      // console.log("mounted_tick: Scrolled Up - STATE CHANGE");
+      headerVisible.value = true;
+    }
+
+    // not scrolling
+    else {
+      // console.log("mounted_tick: DO NOTHING");
+    }
+  }, 1000);
+});
+
+// onUnmounted(() => clearInterval());
+
+const customClass = computed(() => {
+  return (headerVisible.value) ? "custom-header" : "custom-header custom-header-hidden";
+});
+
+watch(customClass, () => {
+  console.log(customClass);
+});
 </script>
 
 <template>
-  <UHeader :toggle="false">
+  <div :class="customClass">
+    this is a test
+  </div>
+
+  <!-- <div :class="customClass"> -->
+  <UHeader
+    :toggle="false"
+    :class="customClass"
+  >
     <template #title>
       <p class="title-my-name">
         JONCORV
       </p>
       <p class="title-beta">
         BETA v0.29
+      </p>
+      <p class="title-beta">
+        scroll y = {{ headerVisible }}
       </p>
     </template>
 
@@ -73,9 +134,26 @@ const items = ref<DropdownMenuItem[]>([
       </UDropdownMenu>
     </template>
   </UHeader>
+  <!-- </div> -->
 </template>
 
 <style scoped>
+.custom-header {
+  position: fixed;
+  z-index: 1001;
+  transform: translateY(0);
+  height: 4rem;
+  width: 100lvw;
+  background-color: green;
+  transition: all 0.3s ease;
+}
+
+.custom-header-hidden {
+  transform: translateY(-100%);
+  background-color: red;
+
+}
+
 .title-my-name {
   font-family: var(--font-mono);
   /* font-family: "Space Mono", monospace; */
