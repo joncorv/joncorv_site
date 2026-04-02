3173de70455344435ef796a89c1c7ff201f9c6d4
Jonathan E. Corriveau <joncorv@gmail.com>
2026-04-01 19:21:10 -0500

    ProjectPagination component mostly completed

---
+++ app/components/ProjectPagination.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+const path = useRoute().path;
+
+const { data: surround_data } = await useAsyncData(`surround-${path}`, () => {
+  return queryCollectionItemSurroundings("animation", path);
+});
+
+console.log("surround_data", surround_data.value);
+</script>
+
+<template>
+  <div class="pagination-container">
+    <NuxtLink v-if="surround_data?.[0]" :to="surround_data?.[0].path" variant="link">
+      <span class="pagination-item">
+        <Icon name="lucide:move-left" class="mr-2" />
+        <span class="pagination-item-small">PREVIOUS PROJECT</span>
+        <br>
+        <span class="pagination-item-large">{{ surround_data?.[0].title }}</span>
+      </span>
+    </NuxtLink>
+
+    <div class="pagination-space" />
+
+    <NuxtLink v-if="surround_data?.[1]" :to="surround_data?.[1].path" variant="link">
+      <div class="pagination-item pagination-item-right">
+        <span class="pagination-item-small">NEXT PROJECT</span>
+        <Icon name="lucide:move-right" class="ml-2" />
+        <br>
+        <span class="pagination-item-large">{{ surround_data?.[1].title }}</span>
+
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.pagination-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pagination-space {
+  flex: 1;
+  background-color: red;
+}
+
+.pagination-item{
+  flex: 0;
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.pagination-item-large {
+  font-size: 2rem;
+  font-weight: 600;
+  font-family: var(--font-serif);
+  color: var(--ui-text-accented);
+}
+.pagination-item-small {
+  font-size: 0.85rem;
+  font-weight: 600;
+  font-family: var(--font-mono);
+  color: var(--ui-text-muted);
+}
+
+.pagination-item-right {
+  text-align: right;
+}
+
+.pagination-item:hover .pagination-item-large {
+  text-decoration: underline;
+  text-decoration-thickness: 1.5px;
+  text-underline-offset: 3px;
+}
+</style>
